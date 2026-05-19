3a9507ec0a8ef760dd2ac7c3429d42d3d77cbcb6
paulicemann <i@pauliceman.com>
2026-05-19 17:24:58 +0300

    LOL updated added

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -3,7 +3,7 @@ import './HelpBox.css';
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-      <h1>LOL</h1>
+      <h1>LOL updated</h1>
       <h2>{title}</h2>
       <p>{text}</p>
     </article>
