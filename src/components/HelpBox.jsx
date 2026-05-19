bd0d0abfb5cd45c89bcdb3eb63efd3bcc35cef3b
paulicemann <i@pauliceman.com>
2026-05-19 17:12:42 +0300

    h1 added instead h2

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -3,6 +3,7 @@ import './HelpBox.css';
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
+      <h1>LOL</h1>
       <h2>{title}</h2>
       <p>{text}</p>
     </article>
